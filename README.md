445a8af06c2e9ba1de5c100552d7fc6db24e907f
Gustly7 <Gustly@yandex.ru>
2022-12-05 03:12:32 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-zm1gpt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-zm1gpt)
---
+++ script.js
@@ -0,0 +1,5 @@
+console.log('Hello!');
+const qwe = document.querySelector(
+  'body > div:nth-child(3) > main > div.WorkSpace-root-PFh_S > div.WorkSpace-appMosaic-VgL9W.mosaic.mosaic-drop-target > div.mosaic-root > div:nth-child(4)'
+);
+console.warn('Hello!2', qwe);
